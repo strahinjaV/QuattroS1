568ae5162fcbc6917dbc9da6576a33e63482b142
Strahinja Vidovic <strahinjav98@gmail.com>
2023-11-27 22:03:51 -0500

    got the custom font added

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="custom-font">Hello Love</div>
+</template>
+
+<style scoped>
+  .custom-font {
+    font-family: AudiType-Normal, sans-serif;
+    font-size: 32px;
+  }
+</style>
